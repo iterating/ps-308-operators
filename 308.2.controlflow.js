a3809152b9d6bf5e32b544940f6d6b2880e592ba
Jonathan Young <jy>
2024-09-12 11:44:34 -0700

    2 methods to solve each problem

---
+++ 308.2.controlflow.js
@@ -0,0 +1,39 @@
+const PI = 3.1415;
+// radius in meters
+const radius = 5;
+// area in sq meters 
+const totalArea = PI * radius * radius
+const plantSpace = 0.8
+let plantCount = []
+let plantsTotalSpaceNow
+let plantsTotalSpacePrevious
+let plantCountPrevious = plantCount[i]
+let week = ['']
+let plantsNow = 20
+let plantTotalSpace = plantsNow * plantSpace
+plantCountNow = plantCountPrevious * 2
+
+
+// set up tests
+console.log(totalArea)
+console.log(plantTotalSpace)
+console.log(plantCountNow)
+console.log(plantCountPrevious)
+
+
+
+plantCount = [20]
+week = [1,2,3,4,5]
+for week[i] (let i = 0; i < week.length; i++) {
+    plantCount.push(plantCountPrevious*2);
+    plantTotalSpaceNow = plantCountNow[i]*0.8;
+    //calculate area
+    console.log(plantCount);
+    console.log(plantTotalSpaceNow);
+    
+}
+
+    
+
+
+//monitored
